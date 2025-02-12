c358713dee380c56cc9ab2ea203dd3a6327d3d86
Artsiom <artsiom.hurynovich@gmail.com>
2025-02-12 12:42:09 +0100

    Initial Commit

---
+++ ColorSlider/ColorSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorSliderApp.swift
+//  ColorSlider
+//
+//  Created by Artem H on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ColorSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
